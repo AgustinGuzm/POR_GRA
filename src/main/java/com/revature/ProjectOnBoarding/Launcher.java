62efaf95cbffd6a5be3bd26beb047fc68ca27f31
Agustin Guzman <agustin.guzman@outlook.com>
2022-05-03 09:31:01 -0400

    first practice of PO

---
+++ src/main/java/com/revature/ProjectOnBoarding/Launcher.java
@@ -0,0 +1,16 @@
+package com.revature.ProjectOnBoarding;
+
+import weekOne.InputData;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+
+		InputData LList = new InputData();
+		LList.IntersectionData();
+		LList.LoopDectectedData();
+		
+
+	}
+
+}
